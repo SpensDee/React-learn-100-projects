4aeb59dfc15f38a60a80757ed8867d5b0cab13a1
Spenes Dee <spens.dee@levelupcasino.com>
2023-05-27 00:02:32 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ let [count ,setCount] = useState(0)
 const onClickMinus = () => setCount(count && --count)
 const onClickPlus = () => setCount(++count)
 
+
   return (
     <div className="App">
       <div>

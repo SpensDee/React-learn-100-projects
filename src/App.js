50ef6b132a98396093be77ebc88cba5cd597b6a6
Spenes Dee <spens.dee@levelupcasino.com>
2023-05-27 09:31:10 +0300

    modal 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { useState } from 'react';
 import './index.scss';
 
 function App() {
-ckPlus = () => setCount(++count)
+  const [open, setOpen] = React.useState(false);
+  const handleClick = () => setOpen(!open);
 
   return (
     <div className="App">
-   
+      <button className="open-modal-btn" onClick={handleClick}>✨ Открыть окно</button>
+      {open && <div className="overlay">
+        <div className="modal">
+          <svg height="200" viewBox="0 0 200 200" width="200" onClick={handleClick}>
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='##'/>
+        </div>
+      </div>}
     </div>
   );
 }

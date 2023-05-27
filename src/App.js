bc8f0b127a0e0cb708219ad5629c5bdc9e9e2467
Spenes Dee <spens.dee@levelupcasino.com>
2023-05-27 08:22:07 +0300

    clear base branch

--- src/App.js
+++ src/App.js
@@ -2,18 +2,11 @@ import React, { useState } from 'react';
 import './index.scss';
 
 function App() {
-let [count ,setCount] = useState(0)
-const onClickMinus = () => setCount(count && --count)
-const onClickPlus = () => setCount(++count)
+ckPlus = () => setCount(++count)
 
   return (
     <div className="App">
-      <div>
-        <h2>Лічильник:</h2>
-        <h1>{ count }</h1>
-        <button className="minus" onClick={onClickMinus}>- Додати</button>
-        <button className="plus" onClick={onClickPlus}>Зменшити +</button>
-      </div>
+   
     </div>
   );
 }
